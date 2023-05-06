c53d3cb444cf91591657c4211218ed85c3ba8369
indra-18 <indraind0918@gmail.com>
2023-05-06 16:09:40 +0530

    created react app with vite and installed tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className=" text-pink-500 font-extrabold text-3xl">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
